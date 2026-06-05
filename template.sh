410f5e1813dd7458d5ab4065045bf785c388b994
tanisha333 <me.tanisha333@gmail.com>
2026-06-05 17:05:38 +0530

    structure

---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p research
+ touch src/__init__.py
+ touch src/helper.py
+ touch src/prompt.py
+ touch .env
+ touch setup.py
+ touch app.py
+ touch requirements.txt
+ touch research/trails.ipynb
+
+ echo "Project structure created successfully!"
