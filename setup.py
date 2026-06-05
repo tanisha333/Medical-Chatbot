25610fdaab08e394779da71eb0c7b7488e5e2d13
tanisha333 <me.tanisha333@gmail.com>
2026-06-05 19:32:26 +0530

    requirement

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Tanisha',
+    author_email='me.tanisha@example.com',
+    packages=find_packages(),
+    install_requires=[]
+)
